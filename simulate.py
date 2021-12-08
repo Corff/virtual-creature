3a8b62a0dcb2b09dfe50e5f944fc887c34ed4681
Marek <marek.tancak16@gmail.com>
2021-12-08 11:34:41 +0000

    shorten runs

--- simulate.py
+++ simulate.py
@@ -4,7 +4,7 @@ from generate import Create_Brain
 def eval(genes):
 	Create_Brain(genes)
 	sim = SIMULATION()
-	return sim.Run(10000)
+	return sim.Run(2500)
 
 if __name__ == "__main__":
 	eval([[0.1 for _ in range(8)] for _ in range(8)])
